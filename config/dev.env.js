bf6f5e9f3f1c69fb028f2f7f9c9179c3aa782d26
zhang.charles <xingwei.zhang@outlook.com>
2023-09-22 16:23:27 +0800

    update host

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://44.203.136.211:8222"',
+  BASE_API: '"http://184.73.109.76:8222"',
 })
